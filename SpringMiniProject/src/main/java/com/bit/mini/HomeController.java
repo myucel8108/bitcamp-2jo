2148104293638a31f4275028e89ba61896bd8ddd
J-LOOK-J <y000945@naver.com>
2022-09-28 16:53:48 +0900

    basic form

--- SpringMiniProject/src/main/java/com/bit/mini/HomeController.java
+++ SpringMiniProject/src/main/java/com/bit/mini/HomeController.java
@@ -7,11 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 	@GetMapping("/")
 	public String home() {
-		return "/bit/layout/main"; //WEB-INF/layout/main.jsp가 메인페이지로
-	}
-	
-	@GetMapping("/help/map")
-	public String map() {
-		return "/bit/help/googlemap";
+		return "/home"; //WEB-INF/layout/main.jsp가 메인페이지로
 	}
 }
